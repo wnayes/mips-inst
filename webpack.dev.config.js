cc288670bf5f00b25eefc23586ed293bfb6cf147
Will Nayes <wnayes@gmail.com>
2018-08-30 20:40:43 -0500

    Fix umd module in node. Remove old bundle.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -7,6 +7,7 @@ module.exports = {
     libraryTarget: "umd",
     library: "MIPSInst",
     filename: "mipsinst.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
+    globalObject: "typeof self !== 'undefined' ? self : this"
   }
 };
